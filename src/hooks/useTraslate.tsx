94a9fdc982cf79e1fbdcf4b54c52f65fac9274d2
Luisma <luismasuarezzz@gmail.com>
2024-07-27 17:52:31 -0400

    add feat useLocale, useTranslate, types, utils to persist locale, change path by locale

--- src/hooks/useTraslate.tsx
+++ src/hooks/useTraslate.tsx
@@ -1,9 +1,15 @@
+import { useCallback } from "react";
 import { useIntl } from "react-intl";
 import { TranslationKey } from "../types";
 
 export const useTranslate = () => {
   const { formatMessage } = useIntl();
-  const t = (key: TranslationKey) => formatMessage({ id: key });
+  const t = useCallback(
+    (key: TranslationKey) =>
+      // Call formatMessage with an object that has an id property set to the given key, as well as any additional arguments passed in.
+      formatMessage({ id: key }),
+    [formatMessage]
+  );
 
   return { t };
 };
